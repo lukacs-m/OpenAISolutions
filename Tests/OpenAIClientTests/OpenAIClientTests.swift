446d18fd70e7d3b1876a362f46b8e7764970e77d
Martin <mlukacs.dev@gmail.com>
2023-02-18 19:16:05 +0100

    clean test

--- Tests/OpenAIClientTests/OpenAIClientTests.swift
+++ Tests/OpenAIClientTests/OpenAIClientTests.swift
@@ -1,11 +1,10 @@
 import XCTest
-@testable import OpenAISolutions
+@testable import OpenAIClient
 
 final class OpenAISolutionsTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(OpenAISolutions().text, "Hello, World!")
     }
 }
